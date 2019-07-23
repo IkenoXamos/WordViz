a92381c1f268591b3403a89090414c33b5e4db3f
Dchunn <chunn289@gmail.com>
2019-07-23 10:34:38 -0500

    added chapter and story junit test

---
+++ WordViz/src/test/java/com/wordviz/test/StoryTest.java
@@ -0,0 +1,104 @@
+package com.wordviz.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StoryTest {
+
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStoryIntegerIntegerStringListOfTagIntegerInteger() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStoryIntegerStringListOfTagInteger() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStoryIntegerStringInteger() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStoryId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetStoryId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAuthor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetAuthor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetVote() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetVote() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTags() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTags() {
+		fail("Not yet implemented");
+	}
+
+}
