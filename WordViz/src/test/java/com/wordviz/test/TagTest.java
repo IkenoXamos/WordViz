aaf25bc93c367652ed3f1ded2986106ae847a47d
MatthewOberlies <moberlies1337@gmail.com>
2019-07-23 10:28:42 -0500

    Generated some blank unit tests

---
+++ WordViz/src/test/java/com/wordviz/test/TagTest.java
@@ -0,0 +1,64 @@
+package com.wordviz.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TagTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTag() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTagString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTagIntegerString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTagId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTagId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ WordViz/src/test/java/com/wordviz/test/UserTest.java
@@ -0,0 +1,89 @@
+package com.wordviz.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUserStringInteger() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUserStringIntegerString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUserIntegerStringIntegerString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUserId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetUserId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUsername() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetUsername() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPassword() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPassword() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDisplayName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDisplayName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
