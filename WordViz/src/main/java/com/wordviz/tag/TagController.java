01865563936a13406e96aa96576319cad5dd6721
Thomas Payton <thomaspayton53@gmail.com>
2019-07-27 13:28:11 -0500

    added /tag mapping

--- WordViz/src/main/java/com/wordviz/tag/TagController.java
+++ WordViz/src/main/java/com/wordviz/tag/TagController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/tag")
 public class TagController {
-
+	
 	@Autowired
 	private TagService tagService;
 	
