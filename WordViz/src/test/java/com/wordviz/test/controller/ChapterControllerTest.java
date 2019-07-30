2924fd2769689c7b351d5808527b82d4fcae2909
Dchunn <chunn289@gmail.com>
2019-07-29 22:20:26 -0500

    Controller Tests are up

--- WordViz/src/test/java/com/wordviz/test/controller/ChapterControllerTest.java
+++ WordViz/src/test/java/com/wordviz/test/controller/ChapterControllerTest.java
@@ -2,7 +2,10 @@ package com.wordviz.test.controller;
 
 import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.sql.Timestamp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,14 +18,21 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wordviz.chapter.Chapter;
 import com.wordviz.chapter.ChapterController;
 import com.wordviz.chapter.ChapterService;
+import com.wordviz.story.Story;
+import com.wordviz.user.User;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(ChapterController.class)
-@DataJpaTest
 public class ChapterControllerTest {
-		
+	
+	@Autowired
+	 private ObjectMapper om;
+	
 	@Autowired
 	private MockMvc mvc;
 	
@@ -30,14 +40,33 @@ public class ChapterControllerTest {
 	private ChapterService service;
 	
 	@Test
-	public void insertChapter() {
+	public void insertChapter() throws JsonProcessingException, Exception {
+		User u = new User("WillOma", 12345, "WilliamOma");
+		Story st = new Story(418, u, "Lord of the Frogs", null, 1, 0);
+		Chapter ch = new Chapter(4585, st, "The Frogs Gather", "IT all started blah blah blah", new Timestamp(System.currentTimeMillis()));
+		mvc.perform(post("/chapter/new")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(om.writeValueAsString(ch)))
+				.andExpect(status().isOk());
 	}
 	
 	@Test
 	public void allChapter() throws Exception {
-		mvc.perform(get("/WordViz/chapter/all")
+		mvc.perform(get("/chapter/all")
 				.contentType(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk());	
+	}
+	
+	@Test
+	public void updateChapter() throws JsonProcessingException, Exception {
+		User u = new User("WillOma", 12345, "WilliamOma");
+		Story st = new Story(418, u, "Lord of the Frogs", null, 1, 0);
+		Chapter ch = new Chapter(4585, st, "The Frogs Gather", "IT all started blah blah blah", new Timestamp(System.currentTimeMillis()));
+		mvc.perform(post("/chapter/update")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(om.writeValueAsString(ch)))
+				.andExpect(status().isOk());
+		
 	}
 
 }
