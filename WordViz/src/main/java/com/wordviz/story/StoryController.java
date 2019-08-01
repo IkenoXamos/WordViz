a8417db9880c44cb05dd49dbff72d845d290a8c1
MatthewOberlies <moberlies1337@gmail.com>
2019-07-31 19:03:16 -0500

    Added ability to update stories

--- WordViz/src/main/java/com/wordviz/story/StoryController.java
+++ WordViz/src/main/java/com/wordviz/story/StoryController.java
@@ -49,4 +49,9 @@ public class StoryController {
 	public List<Story> getStoriesByUser(@RequestBody User user) {
 		return storyService.findByAuthor(user);
 	}
+	
+	@PostMapping(value = "/update")
+	public Story getStoriesByUser(@RequestBody Story story) {
+		return storyService.save(story);
+	}
 }
